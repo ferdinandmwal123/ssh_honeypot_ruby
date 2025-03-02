9d18b019ae67290ad6ee0e570b37ed65af742848
ferdinandmwal123 <ferdinandmwal@gmail.com>
2025-03-02 16:43:41 -0500

    Find available port method

--- sshpot.rb
+++ sshpot.rb
@@ -1,5 +1,21 @@
 require 'socket'
 
-ip = '127.0.0.1' #local
+server_name = 'localhost' #hostname
 
-def main:
+
+def find_available_port #find an available port between 1025 and 65534
+    begin 
+        port = rand(1025..65534) 
+        server = TCPServer.new('localhost', port)
+
+        return port
+    rescue SocketError, Errno::EADDRINUSE => e
+        puts "There has neen an error #{e}"
+        puts "Most likely port no #{port} is already in use"
+        puts "Retrying..."
+        retry
+    end
+end
+
+port_to_use = find_available_port
+puts "Port to use: #{port_to_use}"
